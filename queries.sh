6a31a0e0dd1a061c2d1ae9bbd0230723cd3ac73a
Davide <153355023+zerocool19@users.noreply.github.com>
2024-05-13 20:46:02 +0200

    Update 7 / 12 queries.sh

--- queries.sh
+++ queries.sh
@@ -14,16 +14,16 @@ echo -e "\nAverage number of goals in all games from the winning teams:"
 echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT game_id, AVG(winner_goals + opponent_goals) AS avg_total_goals FROM games GROUP BY game_id")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT name FROM teams FULL JOIN games ON teams.team_id = games.winner_id WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "")"
